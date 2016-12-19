45e65bfbe7cbb2b09f1ef73bd662818f51bc29eb
pasaunders <kaorti@gmail.com>
2016-12-19 13:57:53 -0800

    second tdd switch

--- src/deque.py
+++ src/deque.py
@@ -13,22 +13,27 @@ class Deque(object):
         self.tail_node = self.dll.tail_node
         self.length = 0
 
-    def append(self):
+    def append(self, contents):
+        """Add node to this dll."""
+        self.dll.append(contents)
+        self.head_node = self.dll.head_node
+        self.tail_node = self.dll.tail_node
+        self.length += 1
 
 
-    def append_left(self):
+    # def append_left(self):
 
 
-    def pop(self):
+    # def pop(self):
 
 
-    def pop_left(self):
+    # def pop_left(self):
 
 
-    def peek(self):
+    # def peek(self):
 
 
-    def peek_left(self):
+    # def peek_left(self):
 
 
-    def size(self):
+    # def size(self):
