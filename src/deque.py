8392ac8ab337a9fe832582928e0d8ae5cb8283aa
pasaunders <kaorti@gmail.com>
2016-12-19 13:46:17 -0800

    first test switchback

---
+++ src/deque.py
@@ -0,0 +1,34 @@
+"""Implement a double queue, accessable at head and tail."""
+
+from dll import DoublyLinkedList
+
+
+class Deque(object):
+    """Using a doubly-linked list to build a deque."""
+
+    def __init__(self, iterable=None):
+        """Initialize the deque."""
+        self.dll = DoublyLinkedList(iterable)
+        self.head_node = self.dll.head_node
+        self.tail_node = self.dll.tail_node
+        self.length = 0
+
+    def append(self):
+
+
+    def append_left(self):
+
+
+    def pop(self):
+
+
+    def pop_left(self):
+
+
+    def peek(self):
+
+
+    def peek_left(self):
+
+
+    def size(self):
--- src/test_deque.py
+++ src/test_deque.py
@@ -5,6 +5,7 @@ def test_deque_empty_init():
     """Test for empty queue init."""
     from deque import Deque
     empty_deque = Deque()
-    assert empty_deque.peek() is None
-    assert empty_deque.peekleft() is None
-    assert empty_deque.size() == 0
+    assert empty_deque.length == 0
+    assert empty_deque.head_node is None
+    assert empty_deque.tail_node is None
+    # assert empty_deque.size() == 0
