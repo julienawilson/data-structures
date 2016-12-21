84aa2d28bf3dd56c93232f2384dcd6bc4b951af2
pasaunders <kaorti@gmail.com>
2016-12-21 15:49:13 -0800

    pop test

--- src/test_priority.py
+++ src/test_priority.py
@@ -5,11 +5,11 @@ import pytest
 
 INPUT_VALUES = [
     ([], {}),
-    ([(1, 'a'), (2, 'b'), (3, 'c')], {'1': ['a'], '2': ['b'], '3': ['c']}),
-    ([(2, 'a'), (5, 'b'), (12, 'c')], {'2': ['a'], '5': ['b'], '12': ['c']}),
-    ([(15, 'words'), (6, 'are'), (70, 'hard')], {'6': ['are'], '15': ['words'], '70': ['hard']}),
-    ([(1, 15), (2, 20), (3, 25)], {'1': [15], '2': [20], '3': [25]}),
-    ([(1, 'a'), (1, 'b'), (2, 'c')], {'1': ['a', 'b'], '2': ['c']}),
+    ([(1, 'a'), (2, 'b'), (3, 'c')], {'1': ['a'], '2': ['b'], '3': ['c']}, 'a'),
+    ([(2, 'a'), (5, 'b'), (12, 'c')], {'2': ['a'], '5': ['b'], '12': ['c']}, 'a'),
+    ([(15, 'words'), (6, 'are'), (70, 'hard')], {'6': ['are'], '15': ['words'], '70': ['hard']}, 'are'),
+    ([(1, 15), (2, 20), (3, 25)], {'1': [15], '2': [20], '3': [25]}, 15),
+    ([(1, 'a'), (1, 'b'), (2, 'c')], {'1': ['a', 'b'], '2': ['c']}, 'a'),
 ]
 
 
@@ -18,7 +18,7 @@ def pri(request):
     """Paramaterized fixture for testing."""
     from priority import PriorityQueue
     processed = PriorityQueue(request.param[0])
-    prioq = (processed, request.param[1])
+    prioq = (processed, request.param[1], request.param[2])
     return prioq
 
 
@@ -30,3 +30,7 @@ def test_init(pri):
 def test_insert(pri):
     assert pri[0]._pq_dict == pri[1]
     # (the_input, the_expected_output) = pri
+
+
+def test_pop(pri):
+    assert pri[0].pop() == pri[2]
