ef0dc8e7a338dd361753736d8f4d95f9f36c6158
Rick Valenzuela <devnull@rickv.com>
2017-01-16 13:12:53 -0800

    moved init line

--- src/bst.py
+++ src/bst.py
@@ -1,10 +1,8 @@
 """Classes for binary search tree."""
 
-class Node(self, value, left=None, right=None):
-    def __init__():
+class Node():
+    def __init__(self, value, left=None, right=None):
         """Instantiate a node object."""
         self.value = value
         self.left = left
         self.right = right
-
-
