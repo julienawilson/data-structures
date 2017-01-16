4b9eb658236abed6263f290c913dde5f0d34c101
Rick Valenzuela <devnull@rickv.com>
2017-01-16 13:07:42 -0800

    added Node class

---
+++ src/bst.py
@@ -0,0 +1,10 @@
+"""Classes for binary search tree."""
+
+class Node(self, value, left=None, right=None):
+    def __init__():
+        """Instantiate a node object."""
+        self.value = value
+        self.left = left
+        self.right = right
+
+
