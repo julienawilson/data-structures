56705a048a58f8c08b8f1173c4669fddef9414ba
Rick Valenzuela <devnull@rickv.com>
2017-01-16 14:16:06 -0800

    added fixture, tests for insert

--- src/test_bst.py
+++ src/test_bst.py
@@ -4,6 +4,17 @@ import pytest
 from bst import Node
 from bst import BinarySearchTree
 
+@pytest.fixture()
+def small_tree():
+    """Grow a small tree with five nodes."""
+    tree = BinarySearchTree()
+    tree.insert(50)
+    tree.insert(40)
+    tree.insert(80)
+    tree.insert(35)
+    tree.insert(60)
+    tree.insert(90)
+    return tree
 
 def test_node_creation():
     """Test that instantiating new node creates instance."""
@@ -41,14 +52,36 @@ def test_new_node_right_val():
 def test_new_tree_has_no_root():
     """Test new binary search tree is empty."""
     b_tree = BinarySearchTree()
-    assert  b_tree.size == 0
+    assert b_tree.size == 0
 
 
 def test_insert_in_empty_tree_establishes_root():
     """Test inserting to empty BST assigns value as root."""
     b_tree = BinarySearchTree()
     b_tree.insert(17)
-    assert b_tree.root == 17
+    assert b_tree.root.value == 17
 
 
-def test_insert_
+def test_inserting_lower_val_pushes_left():
+    """Test that inserting lower value creates left branch."""
+    b_tree = BinarySearchTree()
+    b_tree.insert(17)
+    b_tree.insert(10)
+    assert b_tree.root.left.value == 10
+
+
+def test_inserting_higher_val_pushes_right():
+    """Test that inserting higher value creates right branch."""
+    b_tree = BinarySearchTree()
+    b_tree.insert(17)
+    b_tree.insert(20)
+    assert b_tree.root.right.value == 20
+
+def test_inserting_less_but_more_into_populated_tree(small_tree):
+    small_tree.insert(43)
+    assert small_tree.root.left.right.value == 43
+
+
+def test_inserting_lower_item_into_populated_tree(small_tree):
+    small_tree.insert(33)
+    assert small_tree.root.left.left.left.value == 33
