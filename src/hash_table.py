5ca754b9c11f9310aeb8dc52c43c5e04a3e3bda6
Julien <julienawilson@gmail.com>
2017-01-25 13:31:45 -0800

    set and hash functions

--- src/hash_table.py
+++ src/hash_table.py
@@ -8,16 +8,25 @@ class HashTable(object):
         """Initialize a hash table."""
         self._size = size
         self.buckets = [[] for bucket in range(self._size)]
-        self._hash_alg = hash_alg
+        self._hash_alg = self._hash(hash_alg)
 
-    # def _hash(self, hash_alg, word):
-    #     if
-    #     self.word = word
-    #     additive = sum([ord(char) for char in list(word)]) % len(buckets)
+    def _hash(self, hash_alg):
+        if hash_alg == 'additive':
+            return self._additive_hash
+        else:
+            raise ValueError("Please enter a valid hash algorithm.  The options are 'additive'.")
 
-    def _add_hash(self, word):
+    def _additive_hash(self, word):
         """Return Additive hash value."""
         return sum([ord(char) for char in list(word)]) % self._size
 
-
-    # def set(self, key, value)
+    def set(self, key, value):
+        """Set a new key value pair in the has table."""
+        if type(key) is not str:
+            raise TypeError("Key for hash table must be a string.")
+        hash_val = self._hash_alg(key)
+        for pair in self.buckets[hash_val]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.buckets[hash_val].append([key, value])
