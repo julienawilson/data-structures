dd383b76f727f9a6eeb33dcd920df614d7cb0868
Julien <julienawilson@gmail.com>
2017-01-31 10:29:42 -0800

    fixed insertion sort bug

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -8,9 +8,12 @@ def insertion_sort(some_list):
     while idx < len(some_list) - 1:
         if some_list[idx] > some_list[idx + 1]:
             some_list[idx], some_list[idx + 1] = some_list[idx + 1], some_list[idx]
-            for i in some_list[:idx:-1]:
-                import pdb; pdb.set_trace()
-                if some_list[idx] < i:
-                    some_list[idx], i = i, some_list[idx]
+            bw_list = some_list[:idx][::-1]
+            bw_idx = idx
+            for i in range(len(bw_list)):
+                # import pdb; pdb.set_trace()
+                if some_list[bw_idx] < bw_list[i]:
+                    some_list[bw_idx], bw_list[i] = bw_list[i], some_list[bw_idx]
+                bw_idx -= 1
         idx += 1
     return some_list
