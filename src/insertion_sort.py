7f6208a0c0539dc6ba3f678c2fdebaf2b8abe529
Julien <julienawilson@gmail.com>
2017-01-31 14:34:33 -0800

    timit insertion sort

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -17,3 +17,21 @@ def insertion_sort(some_list):
                 bw_idx -= 1
         idx += 1
     return some_list
+
+if __name__ == "__main__":
+
+    import timeit
+    import random
+
+    def build_random_list():
+        """Build a random list to sort."""
+        rand_list = [random.randint(0, 1000) for i in range(200)]
+        return rand_list
+
+    lst = build_random_list()
+
+    print(timeit.repeat(stmt='insertion_sort(lst)',
+                        setup='from __main__ import insertion_sort, lst, random', repeat=3,
+                        number=1000
+                        )
+          )
