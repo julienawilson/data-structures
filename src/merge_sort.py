55420a8e29d423c160dd74d4b7bed4bfe66ce3cf
Rick Valenzuela <devnull@rickv.com>
2017-01-31 14:10:39 -0800

    reverting helper functions final ifs

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,6 +1,7 @@
 """Merge sort implementation."""
 
-def  merge_sort(a_list):
+
+def merge_sort(a_list):
     """Divide, conquer to sort list."""
     if len(a_list) <= 1:
         return a_list
@@ -9,20 +10,19 @@ def  merge_sort(a_list):
     second_half = a_list[midpoint:]
     merge_sort(first_half)
     merge_sort(second_half)
-    return merge(left, right)
+    return merge(first_half, second_half)
+
 
 def merge(list1, list2):
     """The helper function to do the comparisons."""
-    result = [ ]
+    result = []
     while list1 and list2:
         if list1[0] <= list2[0]:
             result.append(list1.pop(0))
         else:
             result.append(list2.pop(0))
     if list1:
-        result += list1
+        result.append(list1.pop(0))
     if list2:
-        result += list2
+        result.append(list2.pop(0))
     return result
-
-
