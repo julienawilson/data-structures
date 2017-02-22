c73980f195b0afab5e72fc1df0436a6c56283ed6
Julien <julienawilson@gmail.com>
2017-02-21 16:17:04 -0800

    find_mean and a test

---
+++ src/test_kmeans.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+
+@pytest.fixture
+def kmc():
+    """Fixture to return a default KMC."""
+    from k_means import KMeansClassifier
+    return KMeansClassifier()
+
+
+@pytest.fixture
+def some_data():
+    """Fixture to return some dummy data."""
+    data = np.array([[2,3],[4,5],[6,7],[8,9],[1,1],[2,2],[3,3],[4,4]])
+    return pd.DataFrame(data=data)
+
+
+def test_find_mean(kmc, some_data):
+    """Unit test for find mean."""
+    data_means = kmc._find_mean(some_data)
+    assert data_means == [3.75, 4.25]
